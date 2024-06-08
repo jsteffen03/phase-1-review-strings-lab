031ffab69bef4e05ca9f447c4cb88c23a35b7fe4
Justin Steffen <jcsteffen03@gmail.com>
2024-06-07 19:48:43 -0600

    Lab Redo

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper'
-
+const currentUser = `Grace Hopper`;
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)} !`
